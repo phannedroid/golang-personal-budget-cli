b30f5c6df92457f4b807d5d25ebbb0183b1bc0ab
phannedroid <phannedroid@gmail.com>
2022-05-08 23:34:38 +0100

    Completed module

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -53,6 +53,11 @@ var errDuplicateEntry = errors.New("Cannot add duplicate entry")
 
 // AddItem adds an item to the current budget
 func (b *Budget) AddItem(description string, price float32) error {
+	if b.CurrentCost()+price > b.Max {
+		return errDoesNotFitBudget
+	}
+	newItem := Item{Description: description, Price: price}
+	b.Items = append(b.Items, newItem)
 
 	return nil
 }
@@ -61,7 +66,8 @@ func (b *Budget) AddItem(description string, price float32) error {
 func (b *Budget) RemoveItem(description string) {
 	for i := range b.Items {
 		if b.Items[i].Description == description {
-
+			b.Items = append(b.Items[:i], b.Items[i+1:]...)
+			break
 		}
 	}
 }
@@ -69,6 +75,13 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
+	newBudget2 := new(Budget)
+	newBudget2.Max = max
+	newBudget = newBudget2
+	keys := make([]time.Month, 0, len(report))
+	for m := range report {
+		keys = append(keys, m)
+	}
 
 	return newBudget, nil
 }
